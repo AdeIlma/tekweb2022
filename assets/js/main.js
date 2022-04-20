55e59fd47caca099e1c98f2df8310e28cb61c38e
AdeIlma <ilmaskuukzky@gmail.com>
2022-04-20 22:48:42 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,58 @@
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+Vue.createApp({
+  data() {
+    return {
+      markdownArticle: null,
+      jsondata: {},
+      link: {},
+    };
+  },
+  methods: {
+    getJsonData() {
+      axios
+        .get('https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json')
+        .then((res) => {
+          // console.log(res.data);
+          this.jsondata = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getMarkdownLink() {
+      var converter = new showdown.Converter();
+      axios
+        .get('https://raw.githubusercontent.com/AdeIlma/tekweb2022/main/assets/contents/article.json')
+        .then((res) => {
+          for (var i of Object.keys(res.data)) {
+            this.link = res.data[i].markdown;
+            console.log(this.link);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async toTheMarkdown() {
+      var converter = new showdown.Converter();
+      const config = {
+        method: 'get',
+        url: 'https://raw.githubusercontent.com/AdeIlma/tekweb2022/main/assets/contents/blackpink.md',
+      };
+
+      let res = await axios(config);
+      var html = converter.makeHtml(res.data);
+      this.markdownArticle = html;
+
+      if (res.status === 200) {
+        console.log('Data Retrieved');
+      }
+      // console.log(this.markdownArticle);
+      // console.log(res.status);
+    },
+  },
+  beforeMount() {
+    this.toTheMarkdown();
+    this.getJsonData();
+    this.getMarkdownLink();
+  },
+}).mount('#app');
